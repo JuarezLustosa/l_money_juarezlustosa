d85a21c26433ab1b871244449589f1c4383dc2b0
Juarez Lustosa <juarez.lustosa@gmail.com>
2020-05-16 15:37:19 -0300

    Add schema and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_15_181502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admin_offers", force: :cascade do |t|
+    t.string "advertiser_name", null: false
+    t.string "url", null: false
+    t.text "description", null: false
+    t.date "start_at", null: false
+    t.date "ends_at"
+    t.boolean "premium"
+    t.string "status", default: "disabled"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Atention this is a fake infos. Only for test on development envirioment
+require 'faker'
+
+if Rails.env.development?
+  ## Enabled premium
+  2.times.each do
+    Admin::Offer.create(
+      advertiser_name: "Advertiser premium #{rand(1..1000)}",
+      description:     Faker::Lorem.paragraph(sentence_count: 2),
+      url:             Faker::Internet.url(host: 'lemmoney.com'),
+      start_at:        Time.current,
+      ends_at:         Time.current + 5.day,
+      premium:         true,
+      status:          :enabled
+    )
+  end
+  ## Enabled non premium
+  5.times.each do
+    Admin::Offer.create(
+      advertiser_name: "Advertiser #{rand(1..1000)}",
+      description:     Faker::Lorem.paragraph(sentence_count: 2),
+      url:             Faker::Internet.url(host: 'lemmoney.com'),
+      start_at:        Time.current,
+      ends_at:         Time.current + 3.day,
+      premium:         false,
+      status:          :enabled
+    )
+  end
+  ## Disabled
+  3.times.each do
+    Admin::Offer.create(
+      advertiser_name: "Advertiser disabled #{rand(1..1000)}",
+      description:     Faker::Lorem.paragraph(sentence_count: 2),
+      url:             Faker::Internet.url(host: 'lemmoney.com'),
+      premium:         false,
+      status:          :disabled
+    )
+  end
+end
